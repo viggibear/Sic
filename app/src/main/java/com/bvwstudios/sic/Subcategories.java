aee5cb08432203e7a9773177d9f19e6984822d4b
Peanut <pwypeanut@gmail.com>
2015-06-27 03:51:16 +0800

    Added in REST API queries without integration.

---
+++ app/src/main/java/com/bvwstudios/sic/Subcategories.java
@@ -0,0 +1,10 @@
+package com.bvwstudios.sic;
+
+class Subcategories {
+    static int POLITICS = 1 << 0;
+    static int SPORTS = 1 << 1;
+    static int ECONOMY = 1 << 2;
+    static int SCIENCE = 1 << 3;
+    static int ENTERTAINMENT = 1 << 4;
+    static int HEALTH = 1 << 5;
+}
